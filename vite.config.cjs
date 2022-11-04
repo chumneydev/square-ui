da84589f487f14810a47b1a001a0a0d47b12899b
Louis Stephens <lstephens@chumneyads.com>
2022-11-04 16:18:44 -0400

    colors added

--- vite.config.cjs
+++ vite.config.cjs
@@ -2,4 +2,25 @@
 export default {
 	// ...
 	root: "src",
+	build: {
+		outDir: "../dist",
+		// Build out only the files that are needed for the app to run => square.js and square.css
+		// Rename the files to be more descriptive
+		// Minify css with no hashing or sourcemaps
+		rollupOptions: {
+			input: {
+				square: resolve(__dirname, "src/square.js"),
+			},
+			output: {
+				entryFileNames: "[name].js",
+				chunkFileNames: "[name].js",
+				assetFileNames: "[name].[ext]",
+				manualChunks(id) {
+					if (id.includes("node_modules")) {
+						return "vendor";
+					}
+				},
+			},
+		},
+	},
 };
